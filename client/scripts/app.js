ede9ec64eead620886f24c319985b82fe70f59d9
climbsrocks <climbsbytes@gmail.com>
2014-12-29 19:55:38 -0800

    Add functionality to filter messages based on room. Refactor chatRefresh code to be DRY.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
   $('.sendButton').on('click', function() {
     var message = {
       'username': userName,
-      'text': $('.input').val(),
-      'roomname': 'default'
+      'text': $('.messageInput').val(),
+      'roomname': $('.roomInput').val() || 'default'
     };
-
+    // ajax call to POST message
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
@@ -40,23 +40,22 @@ $(document).ready(function() {
       return entityMap[s];
     });
   }
-  // callback function for setInterval to receive messages and then append to dom
-  var chatRefresh = function() {
+
+  // abstracted function to GET data based on user input for 'dataObject'
+  var messageGetter = function(dataObject) {
     $.ajax({
-      // format is url + ? + parameter.
-      // - reverses order of parameter "createdAt" to be the most recent messages
-      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      // sets data parameter based on user input above
+      data: dataObject,
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
+        console.log(data)
         //clear messages before appending, or duplicate entries will appear
         $('.messages').html('');
+        //looping through returned data, formatting and appending each data point to the DOM
         for(var i = 0; i < data.results.length; i++) {
           var pTag = "<p class=" + escapeHtml(data.results[i].roomname) + ">" + escapeHtml(data.results[i].username) + ': ' + escapeHtml(data.results[i].text) + ' room: ' + escapeHtml(data.results[i].roomname) + "</p>";
-          // pTag.attr("class",escapeHtml(data.results[i].roomname));
-          // $('.messages').on("click","p",function() {
-          //   var roomID = this.attr('id');
-          // })
           $('.messages').append(pTag);
         }
       },
@@ -64,8 +63,39 @@ $(document).ready(function() {
         console.error('chatterbox: Failed to receive message');
       }
     });
-
   }
-  setInterval(chatRefresh,2000);
 
+  //set roomID to global scope for use in callback functions
+  var roomID;
+  // callback function for setInterval and other
+  var chatRefresh = function() {
+    var dataToPassIn = {
+      order: "-createdAt"
+    }
+    // roomID only gets set when a user clicks on a room
+    if(roomID) {
+      dataToPassIn.where = {
+        roomname: roomID
+      }
+    }
+    messageGetter(dataToPassIn);
+  }
+
+  //initial call to refresh page for new chats
+  var allMessageRefresh = setInterval(chatRefresh,2000);
+
+
+  //right now this is breaking on rooms with a space in their name. it only selects the first word.
+  $('.messages').on("click","p",function() {
+    //chatRefresh logic depends on roomID variable, setting roomID makes subsequent refreshes show only messages from roomID
+    roomID = $(this).attr('class');
+  });
 });
+
+//take user back to overall lobby
+//give user a list of rooms to choose from
+//add in new input box to let the user choose room to send message to
+//
+//let user "friend" other users and bold their messages
+//
+//backbone
